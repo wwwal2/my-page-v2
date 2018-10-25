9b2e7e10847500403c902ae412846f7b1e047036
wwwal2 <smalex9@gmail.com>
2018-10-25 12:15:48 +0300

    rebuilding app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 
 import { tabNames } from './store/data';
 
@@ -11,29 +8,11 @@ class Footer extends Component {
     value: 0
   };
   
-  changeTab = (event, value) => {
-    this.setState({ value });
-  };
-
   render() {
-    const renderTabs = tabNames.map((item) => {
-      return (
-        <Tab label={item} />
-      )
-    })
+
     return (
       <div>
-        <AppBar position="static">
-          <Tabs
-          onChange={this.handleChange}
-          value={0}
-          inducatorColor="primary"
-          textColor="inherit"
-          centered
-          >
-            {renderTabs}
-          </Tabs>
-        </AppBar>   
+        footer
       </div>
     );
   }
--- src/components/Page1.js
+++ src/components/Page1.js
@@ -1,34 +1,11 @@
 import React, { Component } from 'react';
-import { Grid, Paper } from '@material-ui/core';
 
 class Page1 extends Component {
   render() {
-    const style = {
-      Paper: { paddingTop: 10, textAlign: 'center' }
-    }
+    
     return (
       <div>
-        <Grid container>
-          <Grid item xs={1}></Grid>
-          <Grid container xs={5}>
-            <Grid item xs={6}>
-              <Paper style={style.Paper}>
-              cscsc
-              </Paper>
-            </Grid>
-            <Grid item xs={6}>
-              <Paper style={style.Paper}>
-              cscsc
-              </Paper>
-            </Grid>
-          </Grid>
-          <Grid item xs={5}>
-              <Paper style={style.Paper}>
-              cscsc
-              </Paper>
-          </Grid>
-          <Grid item xs={1}></Grid>
-        </Grid>
+        page
         
       </div>
     );
